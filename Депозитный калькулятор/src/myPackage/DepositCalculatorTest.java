0fc843495649fb240d8f011243224a5abfc085c4
Max <Max@MaxPC>
2020-02-18 00:49:49 +0500

    Test class update

--- Депозитный калькулятор/src/myPackage/DepositCalculatorTest.java
+++ Депозитный калькулятор/src/myPackage/DepositCalculatorTest.java
@@ -8,7 +8,7 @@ class DepositCalculatorTest {
 	@Test
 	void testGetProfit() {
 		Assert.assertEquals("100,00", DepositCalculator.getProfit("1000", "1 год", "10"));
-		Assert.assertEquals("200,00", DepositCalculator.getProfit("2000", "1 год", "10"));
+		Assert.assertEquals("400,00", DepositCalculator.getProfit("2000", "2 года", "10"));
 		Assert.assertEquals(DepositCalculator.ERROR_MESSAGE, DepositCalculator.getProfit("2000", "qwe", "10"));
 		Assert.assertEquals(DepositCalculator.ERROR_MESSAGE, DepositCalculator.getProfit("", "", ""));
 	}
